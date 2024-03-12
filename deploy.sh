09730206c45f95f61145e03fb215cbaba634b279
Theophilus Annor <annor_theophilus@hotmail.com>
2024-03-12 12:52:10 +0000

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+sudo apt install gnome-terminal
+curl https://desktop.docker.com/linux/main/amd64/139021/docker-desktop-4.28.0-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64&_gl=1*nto8hs*_ga*MTQyMzA3MzQ4OC4xNzEwMjQ0OTE4*_ga_XJWPQMJYHQ*MTcxMDI0NDkxNy4xLjEuMTcxMDI0Nzc0MC40My4wLjA.
+sudo apt-get update
+sudo apt-get install ./docker-desktop-4.28.0-amd64.deb
